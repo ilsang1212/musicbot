b573b4e1af6654b1f0fdd26311142b6764e9ead3
ilsang1212 <58781790+ilsang1212@users.noreply.github.com>
2020-04-16 20:52:26 +0900

    Update musicbot.py

--- musicbot.py
+++ musicbot.py
@@ -67,7 +67,7 @@ ytdlopts = {
 }
 
 ffmpegopts = {
-	'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+	'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 30',
 	'options': '-vn'
 }
 
