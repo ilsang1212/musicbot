e75fd99c8581f80150387d66eaeffa95da55f705
ilsang1212 <58781790+ilsang1212@users.noreply.github.com>
2020-04-20 17:31:44 +0900

    Update musicbot.py

--- musicbot.py
+++ musicbot.py
@@ -474,6 +474,7 @@ async def on_ready():
 	print(bot.user.name)
 	print(bot.user.id)
 	print("===========")
+	await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name=command[10][0], type=1), afk = False)
 
 @bot.event
 async def on_command_error(ctx, error):
